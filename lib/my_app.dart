27dccab094e36c1e72f102cd3a964b2b0df4bcff
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-08-15 01:28:51 +0600

    Update conflicts

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,10 +1,6 @@
 import 'package:flashcard/core/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
