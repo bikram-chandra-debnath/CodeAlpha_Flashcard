0bcc3b28d48a4ecd8bc271746f6fc8927c7ef8eb
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-08-17 15:07:17 +0600

    setup my app

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,49 +1,24 @@
-import 'package:flashcard/core/common/widgets/custom_circular_button.dart';
 import 'package:flashcard/core/utils/theme/theme.dart';
+import 'package:flashcard/features/home/bloc/card/card_bloc.dart';
 import 'package:flashcard/features/home/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flashcard',
-      theme: AppTheme.lightMode,
-      darkTheme: AppTheme.darkMode,
-      themeMode: ThemeMode.dark,
-      debugShowCheckedModeBanner: false,
+    return BlocProvider(
+      create: (context) => CardBloc(),
+      child: MaterialApp(
+       
+        title: 'Flashcard',
+        theme: AppTheme.lightMode,
+        darkTheme: AppTheme.darkMode,
+        themeMode: ThemeMode.dark,
+        debugShowCheckedModeBanner: false,
 
-      home: Navigations(),
-    );
-  }
-}
-
-class Navigations extends StatelessWidget {
-  const Navigations({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: HomePage(),
-      floatingActionButton: AppCircularBtn(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Spacer(),
-            IconButton(onPressed: () {}, icon: Icon(Icons.home)),
-            Spacer(),
-
-            Spacer(),
-            IconButton(onPressed: () {}, icon: Icon(Icons.book)),
-            Spacer(),
-          ],
-        ),
+        home: HomePage(),
       ),
     );
   }
