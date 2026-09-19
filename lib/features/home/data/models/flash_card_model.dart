bda9c3374ad5aab0fcfe5cbbfa30d4c22ca05c7c
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-09-19 13:37:26 +0600

    Update FlashCard Model

--- lib/features/home/data/models/flash_card_model.dart
+++ lib/features/home/data/models/flash_card_model.dart
@@ -1,62 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FlashCardModel {
   final int id;
   final String question;
   final String answer;
-  final ReactionModel reactions;
 
   const FlashCardModel({
     required this.id,
     required this.question,
     required this.answer,
-    required this.reactions,
   });
+
 
   factory FlashCardModel.fromMap(Map<String, dynamic> map) {
     return FlashCardModel(
       id: map['id'] ?? 0,
       question: map['question'] ?? '',
       answer: map['answer'] ?? '',
-      reactions: ReactionModel.fromMap(
-        map['reactions'] ?? {},
-      ),
     );
   }
 
+  
+  factory FlashCardModel.fromDocument(DocumentSnapshot doc) {
+    final map = doc.data() as Map<String, dynamic>? ?? {};
+    return FlashCardModel.fromMap(map);
+  }
+
+  
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'question': question,
       'answer': answer,
-      'reactions': reactions.toMap(),
     };
   }
 }
 
-class ReactionModel {
-  final int like;
-  final int love;
-  final int helpful;
 
-  const ReactionModel({
-    required this.like,
-    required this.love,
-    required this.helpful,
-  });
-
-  factory ReactionModel.fromMap(Map<String, dynamic> map) {
-    return ReactionModel(
-      like: map['like'] ?? 0,
-      love: map['love'] ?? 0,
-      helpful: map['helpful'] ?? 0,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'like': like,
-      'love': love,
-      'helpful': helpful,
-    };
-  }
-}
